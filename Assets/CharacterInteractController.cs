a23e7fc7c7a927faea570421b4fe2c7b591642f3
SmeN3 <pawcio5391@gmail.com>
2024-11-25 15:28:26 +0100

    Dodanie klasy bazowej dla interaktywnych przedmiot, dodanie skrzynki

---
+++ Assets/CharacterInteractController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteractController : MonoBehaviour
+{
+    character_controler2d character_controller;
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float offsetDistance = 1f;
+    [SerializeField] float sizeOfInteractableArea = 1.2f;
+    Character character;
+    private void Awake()
+    {
+        character_controller = GetComponent<character_controler2d >();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        character =  GetComponent<Character>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        Vector2 position = rigidbody2d.position + character_controller.lastMotionVector * offsetDistance;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach(Collider2D c in colliders)
+        {
+            Interactable hit = c.GetComponent<Interactable>();
+            if (hit != null)
+            {
+                hit.Interact(character);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/LootContainerInteract.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootContainerInteract : Interactable   
+{
+    [SerializeField] GameObject Chest_Closed;
+    [SerializeField] GameObject Chest_Opened;
+    [SerializeField] bool isOpen;
+    public override void Interact(Character character)
+    {
+    if (!isOpen) 
+        {
+            isOpen = true;
+            Chest_Closed.SetActive(false);
+            Chest_Opened.SetActive(true);
+        }
+    }
+}
