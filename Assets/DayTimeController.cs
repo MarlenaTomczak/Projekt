7428bf78ac255a947ecc1922461cda4a46a9b04e
Dominika <domido56@gmailcom>
2024-11-26 18:09:13 +0100

    Zmiana oswietlenia (cykl dzien/noc)
    - zmiana wygladu wyswietlanego czasu

--- Assets/DayTimeController.cs
+++ Assets/DayTimeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine.UI;
 
 public class DayTimeController : MonoBehaviour
@@ -15,16 +16,38 @@ public class DayTimeController : MonoBehaviour
    [SerializeField] float timeScale = 60f;
 
    [SerializeField] Text text;
+   [SerializeField] Light2D globalLight;
+   private int days;
 
    float Hours 
    {
       get { return time / 3600f; }
    }
 
+   float Minutes 
+   {
+      get { return time % 3600f / 60f; }
+   }
+
    private void Update()
    {
       time += Time.deltaTime * timeScale;
-      text.text = Hours.ToString();
+      int hh = (int)Hours;
+      int mm = (int)Minutes;
+      text.text = hh.ToString("00") + ":" + mm.ToString("00"); 
+      float v = nightTimeCurve.Evaluate(Hours);
+      Color c = Color.Lerp(dayLightColor, nightLightColor, v);
+      globalLight.color = c;
+      if (time > secondsInDay)
+      {
+         NextDay();
+      }
+   }
+
+   private void NextDay()
+   {
+      time = 0;
+      days += 1;
    }
 
 }
