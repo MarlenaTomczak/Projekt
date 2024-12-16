fd0daa6e604bd5ded109e35b4be0766f360419a4
Wika Kos <wiktoria.kostylaa@wp.pl>
2024-12-16 13:00:47 +0100

    przeniesienie skryptow do folderu scripts

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum TransitonType
+{
+    Warp,
+    Scene
+}
+public class Transition : MonoBehaviour
+{
+    [SerializeField] TransitonType transitionType;
+    [SerializeField] string sceneNameToTransition;
+    [SerializeField] Vector3 targetPosition;
+    Transform destination;
+
+    internal void InitiateTransition(Transform toTransition)
+    {
+        switch (transitionType)
+        {
+            case TransitonType.Warp:
+                Cinemachine.CinemachineBrain currentCamera = Camera.main.GetComponent<Cinemachine.CinemachineBrain>();
+                currentCamera.ActiveVirtualCamera.OnTargetObjectWarped(toTransition, destination.position - toTransition.position);
+                toTransition.position = new Vector3(destination.position.x, destination.position.y, toTransition.position.z);
+                break;
+            case TransitonType.Scene:
+                GameSceneManager.instance.InnitSwitchScene(sceneNameToTransition, targetPosition);
+                break;
+        }
+
+    }
+
+    void Start()
+    {
+        destination = transform.GetChild(1);
+    }
+}
