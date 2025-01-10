81108e44b0492f0b14cc116f6c5a0bbea57ce37c
MT12081963 <140328039+MT12081963@users.noreply.github.com>
2025-01-10 09:37:13 +0100

    sen, potrzeby, naprawa bugow

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour
+{
+    public Slider healthSlider; 
+    public Slider energySlider;  
+    public float maxHealth = 100f; 
+    public float maxEnergy = 100f;
+    public float deductionAmount = 30f; 
+
+    public Image regenerateImage; 
+    public Image deductImage;     
+    public float imageDisplayTime = 5f; 
+
+    void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        energySlider.maxValue = maxEnergy;
+        healthSlider.value = maxHealth;
+        energySlider.value = maxEnergy;
+
+        regenerateImage.gameObject.SetActive(false);
+        deductImage.gameObject.SetActive(false);
+    }
+
+    public void FullyRegenerate()
+    {
+        healthSlider.value = maxHealth;
+        energySlider.value = maxEnergy;
+
+        StartCoroutine(DisplayImageForTime(regenerateImage));
+    }
+
+    public void DeductStats()
+    {
+        healthSlider.value = Mathf.Max(healthSlider.value - deductionAmount, 0f); 
+        energySlider.value = Mathf.Max(energySlider.value - deductionAmount, 0f);
+        StartCoroutine(DisplayImageForTime(deductImage));
+    }
+
+    private IEnumerator DisplayImageForTime(Image imageToDisplay)
+    {
+        imageToDisplay.gameObject.SetActive(true); 
+        yield return new WaitForSeconds(imageDisplayTime); 
+        imageToDisplay.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/bed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class bed : MonoBehaviour
+{
+    public PlayerStats playerStats; 
+
+    private void OnMouseDown()
+    {
+        if (playerStats != null)
+        {
+            playerStats.FullyRegenerate();
+        }
+    }
+}
